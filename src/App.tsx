d1747142b441c6af355f05371ffdd929f4ba8cdc
Fredson <85347233+fredsons@users.noreply.github.com>
2021-10-07 17:02:01 -0300

    adiciona funcao map

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
+import { useState } from 'react';
+
 // Importando o Header criado
 import {Header} from './components/Header';
 // Importando o Footer criado
 import { Footer } from './components/Footer';
+
+
+
 // Componente React
 const App = () => {  
-  let name = "Fred";
-  let age = 31;
+  const [name, setName] = useState('Fred');
+  const [age, setAge] = useState(90);
+  const [bg, setBg] = useState('#FF0000');
 
-  const handleClick = () => { // função para o click
-    alert("Clicou na função.");
+  const [list, setList] = useState([
+    "O rato roeu a roupa do rei de roma",
+    "Aqui vai uma frase poética",
+    "a inspiração é pouca"
+  ]);
+
+  const handle20 = () => { // função para o click com useState
+    setAge(20);
+    setBg("#00FF00")
+  }
+
+  const handle90 = () => { // função para o click com useState
+    setAge(90);
+    setBg("#FF0000")
   }
 
   return ( // Usando prop no Header
-    <div>
+    <div style={{backgroundColor: bg}}>
 
       <Header name={name} age={age} />  
+
+      {age === 90 &&
+        <button onClick = {handle20}>Tenho 20 anos </button> // condição do botão
+      }
       
-      <button onClick = {handleClick}> Clique aqui </button> 
+      {age === 20 &&
+        <button onClick = {handle90}>Tenho 90 anos </button>
+      }
+
+      <hr />
+
+      <ul>
+        {list.map((frase, index)=>(
+          <li key={index}>{frase}</li>
+        ))}
+      </ul>
+      
     
 
       <Footer/> 
